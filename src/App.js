320b12d824ec4059184f1291203df127348e7079
Sibina Stoyanova <stoyanovasibina@gmail.com>
2023-02-17 22:09:15 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { useState} from "react";
+
+
+import Header from "./compontents/Header";
+import WelcomeWorld from "./compontents/WelcomeWorld";
+// import Login from "./compontents/Login";
+// import Register from "./compontents/Register";
+// import EditGame from "./compontents/EditGame";
+// import GameDetails from "./compontents/GameDetails";
+import GameCatalog from "./compontents/GameCatalog";
+import CreateGame from "./compontents/CreateGame";
+
+function App() {
+  const [page, setPage] = useState('/home');
+
+  const routes = {
+    '/home': <WelcomeWorld />,
+    '/games': <GameCatalog />,
+    '/create-game': <CreateGame />,
+  }
+
+  const navigationChangeHandler = (path) => {
+    
+    setPage(path);
+    
+  };
+
+  return (
+    <div id="box">
+
+      <Header
+        navigationChangeHandler={navigationChangeHandler}
+      />
+
+      <main id="main-content">
+        {routes[page] || <h2>No Page Found!</h2>}
+      </main>
+
+      {/* <Login />
+      <Register /> */}
+      <CreateGame />
+      {/* <EditGame /> */}
+      {/* <GameDetails /> */}
+      <GameCatalog />
+
+    </div>
+
+  );
+}
+
+export default App;
