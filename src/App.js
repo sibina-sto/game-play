7d8917f75569b49fc101383b8c0751ed980d7da5
Sibina Stoyanova <stoyanovasibina@gmail.com>
2023-02-19 15:39:47 +0200

    game services

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState} from "react";
+import { useState, createElement} from "react";
 
 import Header from "./compontents/Header";
 import WelcomeWorld from "./compontents/WelcomeWorld";
--- src/compontents/GameCatalog/GameCatalog.js
+++ src/compontents/GameCatalog/GameCatalog.js
@@ -1,25 +1,30 @@
-import { useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
+
+import * as gameService from '../../services/gameService';
 
 import GameCard from './GameCard';
 
 const GameCatalog = () => {
-    const [games, setGames] = useState([])
+    const [games, setGames] = useState([]);
+
     useEffect(() => {
-        fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
-            .then(res => res.json())
-            .then(result => {
-                setGames(result)
-            })
-    },[]);
-        
+        setTimeout(() => {
+            gameService.getAll()
+                .then(result => {
+                    setGames(result);
+                });
+        }, 1000);
+    }, []);
+
     return (
-    
         <section id="catalog-page">
             <h1>All Games</h1>
-            {games.map(x => <GameCard game={x} />)}
-            <h3 className="no-articles">No articles yet</h3>
+            {games.length > 0
+                ? games.map(x => <GameCard key={x._id} game={x} />)
+                : <h3 className="no-articles">No articles yet</h3>
+            }
         </section>
-    
+
     );
 };
 
